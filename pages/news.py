1190653524b9929d09519ed4511c28aaaca7cea5
Roman Ščerbak <roma.shcherbak1205@gmail.com>
2025-03-31 11:43:31 +0300

    Likelihood logic and textwrap.

--- pages/news.py
+++ pages/news.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import base64
+import textwrap
+from random import choices
 from utils.frontend_utils import load_data
 
 IMAGE_SIZE = [200, 200]
@@ -19,7 +21,6 @@ def convert_due_dates_into_dates(due_dates):
     for date in due_dates:
         date = date.split(" ")
         date_in_days = int(date[0]) * dates_coded[date[1]]
-        print(date_in_days)
         dates_scores.append(date_in_days)
     return dates_scores
 
@@ -54,7 +55,7 @@ def filter_by_country_category(df: pd.DataFrame):
 def filter_and_display_by_time_period(df: pd.DataFrame, period: str):
     filtered_df = df[df["due_date"] == period]
 
-    # filtered_df = filtered_df.sort_values("likelihood")
+    filtered_df = filtered_df.sort_values("likelihood", ascending=False)
 
     if len(filtered_df) != 0:
 
@@ -67,18 +68,20 @@ def filter_and_display_by_time_period(df: pd.DataFrame, period: str):
         col1, col2, col3, col4 = st.columns(4)
         cols = [col1, col2, col3, col4]
 
+        # TODO: Get uuids
+
         for i, row in enumerate(filtered_df.head(4).itertuples()):
             with cols[i]:
                 # Outer container to align both square and text blocks vertically
                 st.markdown(
-                    f'<div style="display:flex; flex-direction:column; justify-content:flex-start; align-items:center; height:400px;">' # TODO: Calculate heights based on text length
+                    f'<div style="display:flex; flex-direction:column; justify-content:flex-start; align-items:center; height:400px; border:1px solid #ddd; border-radius:10px;">' # TODO: Calculate heights based on text length
                     f'<div style="width:{IMAGE_SIZE[0]}px; height:{IMAGE_SIZE[1]}px; padding:10px; display:flex; justify-content:center; align-items:center;">'
                     f'<img src="https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=" alt="Random Image" style="max-width:{IMAGE_SIZE[0]}px; max-height:{IMAGE_SIZE[1]}px; margin-top: 30px">'
                     f'</div>'
                     f'<div style="text-align:center; margin-top:20px;">'
-                    f'<b>Potential event:</b> {row.potential_event[:SHOW_FIRST_SYMBOLS]}<br>'
-                    f'<b>Reasoning:</b> {row.reasoning[:SHOW_FIRST_SYMBOLS]}'
-                    # f'<b>Likelihood:</b> {row.likelihood}'
+                    f'<b>Potential event:</b> {textwrap.shorten(row.potential_event, width=SHOW_FIRST_SYMBOLS, placeholder="...")}<br>'
+                    f'<b>Reasoning:</b> {textwrap.shorten(row.reasoning, width=SHOW_FIRST_SYMBOLS, placeholder="...")}<br>'
+                    f'<b>Likelihood:</b> {row.likelihood}'
                     f'</div>'
                     f'</div>',
                     unsafe_allow_html=True
@@ -90,6 +93,8 @@ st.set_page_config(layout="wide")
 loaded_data = load_data()
 
 df = pd.DataFrame(loaded_data)
+
+df["likelihood"] = choices(range(1,10),k=len(df))
 
 df["due_date_score"] = convert_due_dates_into_dates(df["due_date"])
 
